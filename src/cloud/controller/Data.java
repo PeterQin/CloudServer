2ee8d37298dd60ff755b663edda1b96247467490
Peter Qin <Peter_Qin@live.com>
2016-08-19 22:51:33 +0800

    add spring

---
+++ src/cloud/controller/Data.java
@@ -0,0 +1,23 @@
+package cloud.controller;
+
+public class Data {
+	private String FName;
+
+	public String getFName() {
+		return FName;
+	}
+
+	public void setFName(String fName) {
+		FName = fName;
+	}
+	
+	public int getFAge() {
+		return FAge;
+	}
+
+	public void setFAge(int fAge) {
+		FAge = fAge;
+	}
+
+	private int FAge;
+}
